76945addaa85fc0a919b7540b9b3e68a7d8c7147
Pradeep Kumar <pradeep.kumar@iiitg.ac.in>
2023-09-08 01:29:40 +0530

    new

---
+++ src/components/Projects/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Software App' ?
+            <ToggleButton active value="Software App" onClick={() => setToggle('Software App')}>Software App'S</ToggleButton>
+            :
+            <ToggleButton value="Software App" onClick={() => setToggle('Software App')}>Software App'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Cloud App' ?
+            <ToggleButton active value="Cloud App" onClick={() => setToggle('Cloud App')}>Cloud App'S</ToggleButton>
+            :
+            <ToggleButton value="Cloud App" onClick={() => setToggle('Cloud App')}>CLoud App'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Web App' ?
+            <ToggleButton active value="Web App" onClick={() => setToggle('Web App')}>Web App'S</ToggleButton>
+            :
+            <ToggleButton value="Web App" onClick={() => setToggle('Web App')}>Web App'S</ToggleButton>
+          }
+          <Divider />
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,237 @@
+export const Bio = {
+  name: "Pradeep Kumar",
+  roles: [
+    "Programmer",
+    "Web Developer",
+  ],
+  description:
+    "I am a motivated and skilled programmer with a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/PradeepKumar368?tab=repositories",
+  resume:
+    "https://github.com/PradeepKumar368/Resume/blob/main/Pradeep_CV.pdf",
+  linkedin: "https://www.linkedin.com/in/pradeepkumar68/",
+  
+  insta: "https://www.instagram.com/not_realistic_pradeep/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "PHP",
+        image: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/PHP-logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "PhpMyAdmin",
+        image: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/PhpMyAdmin.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/aws.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/linux.png",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/IIITG.png",
+    school: "Indian Institute of Information Technology, Guwahati",
+    date: "Dec 2020 - Jun 2024",
+    grade: "7.8 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Indian Institute of Information Technology, Guwahati. I have completed 6 semesters and have a CGPA of 7.8. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. ",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Jesus_&_Mary.png",
+    school: "Jesus And Marry School, Bikaner",
+    date: "Apr 2019 - Apr 2020",
+    grade: "90.4%",
+    desc: "I completed my class 12 high school education at Jesus And Marry School, Bikaner, where I studied Science with Mathematics.",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Dayanand_Public.png",
+    school: "Dayanand Public Shcool, Bikaner",
+    date: "Apr 2017 - Apr 2018",
+    grade: "84%",
+    desc: "I completed my class 10 education at Dayanand Public Shcool, Bikaner",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "OnlineFlightBooking-PHP",
+    date: "Aug 2022 - Nov 2022",
+    description:
+      "Created a Software application that is similar to the process of Web Login before Flight Boarding , Implemented a Payment Gateway for the Completion of Ticket & Purchase And Printing the Bording Pass.",
+    image:
+      "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/OnlineFlightBooking.png",
+    tags: [
+      "PHP",
+      "PhpMyAdmin",
+      "CSS",
+      "HTML",
+      "Js",
+      "MySQL",
+      "PhpMailer",
+    ],
+    category: "Software App",
+    github: "https://github.com/PradeepKumar368/Airline-Reservation-System",
+    member: [
+      {
+        name: "Pradeep Kumar",
+        img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Pradeep.jpg",
+        linkedin: "https://www.linkedin.com/in/pradeepkumar68/",
+        github: "https://github.com/PradeepKumar368?tab=repositories",
+      },
+      {
+        name: "S Abhimanyu",
+        img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/HeroImage.jpg",
+        linkedin: "https://www.linkedin.com/in/s-abhimanyu-95356b249/",
+        github: "https://github.com/s-abhimanyu",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Customer feedback Website - PHP",
+    date: "Jan 2022 - March 2022",
+    description:
+      "Hosted a Customer Feedback Website using Amazon-AWS (Amazon Web Services) , Integrated AWS-RDS, EC2 (Elastic-Cloud-Computing) & other components to host the Website.",
+    image:
+      "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/customer.png",
+    tags: [
+      "PHP",
+      "AWS",
+      "AWS-RDS",
+      "MySql",
+      "EC2",
+      "S3 Bucket",
+    ],
+    category: "Cloud App",
+    github: "{https://github.com/PradeepKumar368/Customer_Feedback_Website",
+    member: [
+      {
+        name: "Pradeep Kumar",
+        img: "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Pradeep.jpg",
+        linkedin: "https://www.linkedin.com/in/pradeepkumar68/",
+        github: "https://github.com/PradeepKumar368?tab=repositories/",
+      },
+      {
+        name: "S Abhimanyu",
+        img: "https://raw.githubusercontent.com/s-abhimanyu/Images/main/HeroImage.jpg",
+        linkedin: "https://www.linkedin.com/in/s-abhimanyu-95356b249/",
+        github: "https://github.com/s-abhimanyu/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Software-Microsoft Power Bi",
+    date: "Jan 2022 - March 2022",
+    description:
+      " Data Analysis - Imported Data From Global Marketing Super Store (2016) And Made a Interactive Dashboard With The Help of Microsoft Power-Bi.",
+    image:
+      "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Power-BI.png",
+    tags: ["Power Bi"],
+    category: "Software App",
+    github: "https://github.com/PradeepKumar368/Power---Bi",
+  },
+  {
+    id: 3,
+    title: "Personalizied Portfolio",
+    date: "May 2023 - JUN 2023",
+    description:
+      "Welcome to my portfolio! This project showcases my skills, projects, and experiences as a web developer.",
+    image:
+      "https://raw.githubusercontent.com/PradeepKumar368/Photos/main/Portfolio.png",
+    tags: [
+      "Js",
+      "HTML",
+      "CSS",
+      "ReactJs",
+      "Style-Components"
+    ],
+    category: "Web App",
+    github: "https://github.com/PradeepKumar368/Portfolio",
+  },
+];
